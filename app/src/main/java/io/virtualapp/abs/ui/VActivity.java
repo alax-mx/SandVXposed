3fd4fef22c849919ca13cea182c447be81a84a8f
alax-mx <lyf378015683@aliyun.com>
2022-03-10 20:46:40 +0800

    新增加载/data/local/tmp/libtest.so的代码

---
+++ app/src/main/java/io/virtualapp/abs/ui/VActivity.java
@@ -0,0 +1,69 @@
+package io.virtualapp.abs.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.flurry.android.FlurryAgent;
+
+import org.jdeferred.android.AndroidDeferredManager;
+
+import io.virtualapp.abs.BaseView;
+
+/**
+ * @author Lody
+ */
+public class VActivity extends AppCompatActivity {
+
+    /**
+     * Implement of {@link BaseView#getActivity()}
+     */
+    public Activity getActivity() {
+        return this;
+    }
+
+    /**
+     * Implement of {@link BaseView#getContext()} ()}
+     */
+    public Context getContext() {
+        return this;
+    }
+
+    protected AndroidDeferredManager defer() {
+        return VUiKit.defer();
+    }
+
+    public Fragment findFragmentById(@IdRes int id) {
+        return getSupportFragmentManager().findFragmentById(id);
+    }
+
+    public void replaceFragment(@IdRes int id, Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(id, fragment).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FlurryAgent.onStartSession(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FlurryAgent.onEndSession(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+}
